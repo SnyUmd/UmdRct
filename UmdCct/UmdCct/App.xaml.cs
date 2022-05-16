96f2a56d88d233ccbbaf811c6348e1b1b561b7bc
SNY-AC\Acer <shinya.umd@gmail.com>
2022-05-17 01:00:51 +0900

    ソリューション作成

---
+++ UmdCct/UmdCct/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace UmdCct
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private NotifyIconWrapper notifyIcon;
+
+        /// <summary>
+        /// System.Windows.Application.Startup イベント を発生させます。
+        /// </summary>
+        /// <param name="e">イベントデータ を格納している StartupEventArgs</param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            this.ShutdownMode = ShutdownMode.OnExplicitShutdown;
+            this.notifyIcon = new NotifyIconWrapper();
+        }
+    }
+}
